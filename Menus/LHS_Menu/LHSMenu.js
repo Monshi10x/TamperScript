03c6a2e570f0694e4afdadece739de3b89ba67ee
Tristan Cargill <monshi10x@gmail.com>
2024-10-18 18:51:35 +1000

    m

--- Menus/LHS_Menu/LHSMenu.js
+++ Menus/LHS_Menu/LHSMenu.js
@@ -350,7 +350,7 @@ async function initLHSMenu() {
     addItem(GM_getResourceURL("Icon_Download"), "Find", "finder");
     addItem(GM_getResourceURL("Icon_M2"), "Area", "m2");
     addItem(GM_getResourceURL("Icon_Lnm"), "Length", "lnm");
-    addItem(ICON.LED, "LED", "LED");
+    addItem(GM_getResourceURL("Icon_LED"), "LED", "LED");
     addItem(ICON.lightbox, "Lightbox", "lightbox", null, "loadedPredefinedParts");
     addItem(ICON.billboard, "Billboard", "billboard", null, "loadedPredefinedParts");
     addItem(ICON.compare, "Compare", "compare", null, "loadedPredefinedParts");
