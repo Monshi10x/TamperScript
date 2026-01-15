8a9ced74cf1d39fd8971c8bece55ebd77ecf8623
Tristan Cargill <monshi10x@gmail.com>
2026-01-15 22:00:52 +1000

    m

--- Menus/Part_Components/Sheet.js
+++ Menus/Part_Components/Sheet.js
@@ -51,7 +51,7 @@ class Sheet extends Material {
       #currentJoinMethod = "Even Joins";
 
       #sheetPerimeterIsCut = true;
-      #useOverallSVGSize = false;
+      #useOverallSVGSize = true;
       #finalOutputSizesHeader;
       /**
        * @Updated on table changes
