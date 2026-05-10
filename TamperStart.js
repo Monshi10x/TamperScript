84ebdad49a18eddfc58f9643afafac37866eb349
Tristan Cargill <30851789+Monshi10x@users.noreply.github.com>
2026-05-10 19:43:15 +1000

    Fix addJoinLine initialization order in frame visualizer

--- TamperStart.js
+++ TamperStart.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Corebridge Plugin (advanced) github
 // @namespace    http://tampermonkey.net/
-// @version      10.628
+// @version      10.629
 // @description  Corebridge Plugin
 // @author       Tristan Cargill
 // @match        https://sar10686.corebridge.net/SalesModule/Estimates/QuickPrice*
